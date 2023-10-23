22fb7f4031051e23169493598f70d4c5388f6510
cindylrs04 <larasaticindy231@gmail.com>
2023-10-23 14:52:10 +0700

    ArrayRataNilai08Modifikasi

--- ArrayRataNilai08.java
+++ ArrayRataNilai08.java
@@ -2,18 +2,29 @@ import java.util.Scanner;
 public class ArrayRataNilai08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+        int banyak;
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        banyak = sc.nextInt();
+        int[] nilaiMhs = new int[banyak];
+        double jumlahLulus = 0, jumlahtidakLulus = 0, rataLulus, ratatidakLulus;
+        int lulus = 0, tidakLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.println("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = sc.nextInt();
         }
         for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
-        }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata2);
+            if (nilaiMhs[i] > 70) {
+                lulus += 1;
+                jumlahLulus += nilaiMhs[i];
+            } else {
+                tidakLulus += 1;
+                jumlahtidakLulus += nilaiMhs[i];
+            }
+        } 
+        rataLulus = jumlahLulus / lulus;
+        ratatidakLulus = jumlahtidakLulus / tidakLulus;
+        System.out.println("Rata-rata nilai = "+rataLulus);
+        System.out.println("Rata-rata nilai tidak lulus = "+ ratatidakLulus);
     }
 }
