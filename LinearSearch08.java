492e1030d9012def332157c417638c0b47297edf
cindylrs04 <larasaticindy231@gmail.com>
2023-10-25 11:35:26 +0700

    LinearSearch08Modifikasi no 4

--- LinearSearch08.java
+++ LinearSearch08.java
@@ -1,16 +1,29 @@
+import java.util.Scanner;
 public class LinearSearch08 {
     public static void main(String[] args) {
-        
-        int [] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan jumlah elemen array : ");
+        int banyak = sc.nextInt();
+        int[] arrayInt = new int[banyak];
         int hasil = 0;
 
-        for (int i = 0; i < arrayInt.length; i++) {
+        for (int i = 0; i < banyak; i++) {
+           System.out.print("Masukkan elemen array ke-" + i + " : ");
+           arrayInt[i] = sc.nextInt();
+        }
+        System.out.print("Masukkan key yang ingin dicari : ");
+        int key =sc.nextInt();
+        for (int i = 0; i < banyak; i++) {
             if (arrayInt[i] == key) {
                 hasil = i;
+                System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil); 
+                break;               
+            } else {
+                System.out.println("Key tidak ditemukan");
                 break;
             }
         }
-        System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
+        
+        } 
     }    
-}
+
